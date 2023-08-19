6e128bf71c5df716019958e3ca535410cc3cd85a
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-08-19 20:21:34 +0300

    Create_invoices_Table & controller & Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,5 +32,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('invoices',InvoicesController::class);
 
 Route::get('/{page}',[AdminController::class,'index']);
