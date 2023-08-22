cbac65e057e4911a4fe6b65962cd4b37a93e2519
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-08-22 11:24:12 +0300

    Show Data Table invoices

--- app/Models/Invoices.php
+++ app/Models/Invoices.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoices extends Model
 {
@@ -25,4 +26,9 @@ class Invoices extends Model
         'note',
         'payment_Date',
     ];
+
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Sections::class,'section_id');
+    }
 }
